0d7dd43e9e06d35a5c7b9540a0bae8080183e344
TamodeepSarkar <86249468+TamodeepSarkar@users.noreply.github.com>
2022-01-29 19:37:34 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -3,7 +3,26 @@ Webcam.set({
     height : 300,
     image_format : 'png',
     png_quality : 90
-});
+});function check()
+{
+    img = document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+
+}
+
+function gotResult(error,results)
+{
+    if(error)
+    {
+        console.error(error);
+    }
+    else
+    {
+      console.log(results);
+      document.getElementById("result_object_name").innerHTML=results[0].label;
+      document.getElementById("result_object_accuracy").innerHTML=results[0].confidence.toFixed(3);
+    }
+}
 
 camera = document.getElementById("camera");
 Webcam.attach('#camera');
